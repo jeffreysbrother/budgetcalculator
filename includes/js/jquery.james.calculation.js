a9b9a0b03b03f9f52cc40ddb9185da3235f5d8d9
James Cool <jeffreysbrother@gmail.com>
2015-12-11 17:00:55 -0800

    more testing

--- includes/js/jquery.james.calculation.js
+++ includes/js/jquery.james.calculation.js
@@ -322,7 +322,7 @@
                 },
                 // subtract an array (custom function added by James Cool)
                 subtract: function (a){
-                        var total = 0;
+                        var total = $("input[name='sum']").sum("keyup", "#totalSum");
 
                         // loop through the value and total them
                         $.each(a, function (i, v){
